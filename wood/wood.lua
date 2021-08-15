af023ccff4d870aec104e111dc139b4c67f0d62d
graham <graham@ulsterit.com>
2021-08-15 12:40:09 +0100

    added wooden ladder

--- wood/wood.lua
+++ wood/wood.lua
@@ -1,5 +1,12 @@
 local S = minetest.get_translator("wood");
 
+wood.sounds = {
+	footstep = {name = "wood_footstep", gain = 0.3},
+	dig = {name = "wood_dig", gain = 0.5},
+	dug = {name = "wood_dug", gain = 1.0},
+	place = {name = "wood_place", gain = 0.5},
+}
+
 minetest.register_craftitem("wood:stick", {
     description = S("Stick"),
     inventory_image = "stick.png",
@@ -19,9 +26,41 @@ minetest.register_craft({
     burntime = 7,
 })
 
-wood.sounds = {
-	footstep = {name = "wood_footstep", gain = 0.3},
-	dig = {name = "wood_dig", gain = 0.5},
-	dug = {name = "wood_dug", gain = 1.0},
-	place = {name = "wood_place", gain = 0.5},
-}
+minetest.register_node("wood:ladder", {
+	description = S("Wooden Ladder"),
+	drawtype = "signlike",
+	tiles = {"wood_ladder.png"},
+	inventory_image = "wood_ladder.png",
+	wield_image = "wood_ladder.png",
+	paramtype = "light",
+	paramtype2 = "wallmounted",
+	sunlight_propagates = true,
+	walkable = false,
+	climbable = true,
+	is_ground_content = false,
+	selection_box = {
+		type = "wallmounted",
+		--wall_top = = <default>
+		--wall_bottom = = <default>
+		--wall_side = = <default>
+	},
+	groups = {choppy = 2, oddly_breakable_by_hand = 3, flammable = 2},
+	legacy_wallmounted = true,
+	sounds = wood.sounds,
+})
+
+minetest.register_craft({
+	output = "wood:ladder 5",
+	recipe = {
+		{"group:stick", "", "group:stick"},
+		{"group:stick", "group:stick", "group:stick"},
+		{"group:stick", "", "group:stick"},
+	}
+})
+
+minetest.register_craft({
+	type = "fuel",
+	recipe = "wood:ladder",
+	burntime = 7,
+})
+
